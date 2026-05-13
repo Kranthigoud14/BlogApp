0d73fb178762967fd3b82e90fd3885d28b85b929
Kranthigoud14 <kranthigoudbalagoni14@gmail.com>
2026-05-13 14:53:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ config();
 const app = exp();
 //enable cors
 app.use(cors({
-  origin:['https://blog-app-n8bd.vercel.app'],
+  origin:['https://blog-app-n8bd.vercel.app/'],
   credentials:true
 }))
 //add cookie parser middeleware
